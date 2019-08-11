f50a367a0a0d978ff6dd150c4db10c3a49349239
Daniel <rdmejiaa@gmail.com>
2019-08-10 22:31:23 -0600

    [mysql-integration] Create migrations for TipSteerDir column

---
+++ tesisCdagAsobiguaApi/Migrations/20190811041851_AddTipSteerDirColumToShot.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tesisCdagAsobiguaApi.Migrations
+{
+    public partial class AddTipSteerDirColumToShot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TipSteerDir",
+                table: "Shots",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "C");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TipSteerDir",
+                table: "Shots");
+        }
+    }
+}
